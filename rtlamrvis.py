ced3d0668732a5d4153d1dff339c434c773db239
Brian Cochrane <brian.j.cochrane@gmail.com>
2019-03-02 22:13:37 -0800

    'read JSON objects from input file and remove unnecessary fields

--- rtlamrvis.py
+++ rtlamrvis.py
@@ -1,22 +1,23 @@
 import json
 
-rtlamr_file = "testdata/test.json"
+rtlamr_file = "testdata/rtlamr_20190130.json"
 
 rtlamr_data = []
 with open(file=rtlamr_file) as rtlamr_f:
     for line in rtlamr_f:
-        # normalize JSON objects and eliminate unnecessary fields
-        if 'ID' in line:
-            print("found ID key")
+        line_json = json.loads(line)
 
+        #  normalize key names (EndpointID -> ID)
+        if 'EndpointID' in line_json['Message']:
+            line_json['Message']['ID'] = line_json['Message']['EndpointID']
+ 
+        # flatten data structure
+        for key in ('ID', 'Consumption'):
+            line_json[key] = line_json['Message'][key]
 
-        # This isn't working. I have to stop thinking in Perl.
+        # delete fields we don't need
+        for key in ('Offset', 'Length', 'Message'):
+            del line_json[key]
 
-        #if 'ID' in line{'Message'}:
-        #    print("found ID")
-        #elif 'EndpointID' in line{'Message'}:
-        #    print("found EndpointID")
-
-        rtlamr_data.append(json.loads(line))
-
-print(rtlamr_data)
+        # print(line_json)
+        rtlamr_data.append(line_json)
